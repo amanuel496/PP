a236a43a491f73c050449041a83aa2474c415f80
amanuelChukala <71105657+amanuelChukala@users.noreply.github.com>
2021-02-04 04:50:01 -0700

    Updates

---
+++ precache-manifest.ef74fee8f8b4254ab769737bde93f2de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fecfbf776e04b27d9d8b1f64d8b6e0",
+    "url": "/PP/index.html"
+  },
+  {
+    "revision": "54b5495b869d163aafb6",
+    "url": "/PP/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "728bf4fc67e393f59571",
+    "url": "/PP/static/css/main.dddd3844.chunk.css"
+  },
+  {
+    "revision": "54b5495b869d163aafb6",
+    "url": "/PP/static/js/2.aa033894.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/PP/static/js/2.aa033894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728bf4fc67e393f59571",
+    "url": "/PP/static/js/main.d73ee74c.chunk.js"
+  },
+  {
+    "revision": "54cff4f545b2f04644bb",
+    "url": "/PP/static/js/runtime-main.de39b1bb.js"
+  },
+  {
+    "revision": "db0ef15b6b21f2bbf9c9ab345908bbcd",
+    "url": "/PP/static/media/background1.db0ef15b.jpg"
+  },
+  {
+    "revision": "d88a63c583ff71e4bed62d286722a538",
+    "url": "/PP/static/media/profile1.d88a63c5.png"
+  }
+]);
